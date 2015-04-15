c22fb16c6ddc5e4dceebcf9854045ab06fcd8c31
GroupX <d.tosatto@studenti.unibs.it>
2015-04-15 21:21:26 +0200

    commit

---
+++ ProvaGit/src/it/pasquali/paolo/prova/git/MainProva.java
@@ -0,0 +1,19 @@
+package it.pasquali.paolo.prova.git;
+
+import java.util.Scanner;
+
+public class MainProva
+{
+
+	public static void main ( String[] args )
+	{
+		// TODO Auto-generated method stub
+		Scanner scnr = new Scanner(System.in);
+		int number;
+		
+		System.out.println("Inserisci un numero cazzo ");
+		number = scnr.nextInt ();
+		System.out.println("Hai in serito "+number);
+	}
+
+}
